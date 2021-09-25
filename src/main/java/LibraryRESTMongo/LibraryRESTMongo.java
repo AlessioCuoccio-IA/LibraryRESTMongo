5bef894d4791120a0793740cc6f59f468a815fe4
AlessioCuoccio-IA <acuoccio@studenti.apuliadigitalmaker.it>
2021-09-25 12:32:32 +0200

    Initial commit

---
+++ src/main/java/LibraryRESTMongo/LibraryRESTMongo.java
@@ -0,0 +1,15 @@
+package LibraryRESTMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryRESTMongo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryRESTMongo.class, args);
+	}
+
+
+
+}
